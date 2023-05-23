628c555dd8a752d99c2738c6386dc54d4487cba6
Swevosfr <90179383+Swevosfr@users.noreply.github.com>
2023-05-23 12:28:39 +0200

    debugging some code in frontend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,45 +1,33 @@
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import Logo from "../images/logoCaptcha-removebg.png";
-import { Link } from "react-router-dom";
 
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-gray-900">
+    <header className=" bg-gray-900">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
           <p className="-m-1.5 p-1.5">
-            <Link
-              to="/home"
-              className="font-semibold text-indigo-500 hover:text-indigo-300 ml-1"
-            >
-              <img
-                className="h-8 w-auto justify-center align-middle"
-                src={Logo}
-                alt=""
-              />
-            </Link>
+            <span className="sr-only">Your Company</span>
+            <img
+              className="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt=""
+            />
           </p>
         </div>
         <div className="flex flex-1 items-center justify-end gap-x-6">
-          <a
-            href="#"
-            className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900"
-          >
+          <p className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-white">
             Log in
-          </a>
-          <a
-            href="#"
-            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            S'inscrire
-          </a>
+          </p>
+          <p className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Sign up
+          </p>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -63,14 +51,15 @@ export default function Example() {
           <div className="flex items-center gap-x-6">
             <p className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img className="h-8 w-auto" src={Logo} />
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt=""
+              />
             </p>
-            <a
-              href="#"
-              className="ml-auto rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              S'inscrire
-            </a>
+            <p className="ml-auto rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              Sign up
+            </p>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -83,12 +72,12 @@ export default function Example() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="py-6">
-                <a
+                <p
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
-                  Se connecter
-                </a>
+                  Log in
+                </p>
               </div>
             </div>
           </div>
--- client/src/pages/public/login/Login.jsx
+++ client/src/pages/public/login/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import Logo from "../../../images/logoCaptcha-removebg.png";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -41,7 +40,11 @@ export default function Login() {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-slate-900 ">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img className="mx-auto h-20 w-auto" src={Logo} alt="Your Company" />
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+          alt="Your Company"
+        />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
           Connectez-vous à votre compte
         </h2>
