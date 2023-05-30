50e166baae964dea3415175a3ea7128bbc10ec01
Swevosfr <90179383+Swevosfr@users.noreply.github.com>
2023-05-30 11:37:02 +0200

    css changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className=" bg-gray-900">
+    <header className=" bg-gray-900 sticky top-0">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8"
         aria-label="Global"
--- client/src/pages/public/home/Home.jsx
+++ client/src/pages/public/home/Home.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Ghost from "../../../images/reshot-icon-ghost-WV9UDSQTY2.svg";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Coucou</h1>
+    <div className="max-w-screen-xl mx-auto px-4 py-28 gap-1 md:px-8">
+      <div className="space-y-5 max-w-4xl mx-auto text-center">
+        <p className="text-gray-900 mt-10 text-center text-4xl font-bold leading-9 tracking-tight ">
+          <span className="text-transparent bg-clip-text mt-10 text-center text-4xl font-bold leading-9 tracking-tight  bg-gradient-to-r from-[#f5f5f5] to-[#f1f1f1]">
+            Projet captcha
+          </span>
+        </p>
+        <div className="justify-center">
+          <img src={Ghost} className="" alt="Captcha" />
+        </div>
+      </div>
     </div>
   );
 }
