5353a11e76aece41964d2a67cde340435a4fbfd1
Swevosfr <90179383+Swevosfr@users.noreply.github.com>
2023-05-31 09:28:38 +0200

    css navbar changes

--- client/src/components/NavbarUser.jsx
+++ client/src/components/NavbarUser.jsx
@@ -196,7 +196,7 @@ export default function Example() {
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col ">
           {/* Sidebar component, swap this element with another sidebar if you like */}
 
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
+          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 ">
             <div className="flex h-16 shrink-0 items-center">
               <img
                 className="h-8 w-auto"
--- client/src/pages/user/dashboard/Dashboard.jsx
+++ client/src/pages/user/dashboard/Dashboard.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Dashboard() {
   return (
     <div className="py-10 lg:pl-72">
-      <div className="px-4 sm:px-6 lg:px-8">Coucou ici c'est le texte </div>
+      <div className="px-4 sm:px-6 lg:px-8 text-white">
+        Coucou ici c'est le texte{" "}
+      </div>
     </div>
   );
 }
