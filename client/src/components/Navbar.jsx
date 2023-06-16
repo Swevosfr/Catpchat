83c5a07e49d17c315b607ef9c6c1b70f76968398
Swevosfr <90179383+Swevosfr@users.noreply.github.com>
2023-06-16 22:51:19 +0200

    navbar modifications

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Example() {
   return (
     <header className=" bg-gray-900 sticky top-0">
       <nav
-        className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8"
+        className="mx-auto flex items-center justify-between gap-x-6 p-6 lg:px-8"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
