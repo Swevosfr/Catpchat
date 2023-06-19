29efb3910a6b525ad4997813fabfc2fcdfbfc3ea
Swevosfr <90179383+Swevosfr@users.noreply.github.com>
2023-06-19 16:24:16 +0200

    captcha router for displaying images

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use(cors());
 
 //routes
 app.use("/auth", require("./routes/jwtAuth"));
+app.use("/captcha", require("./routes/captcha"));
 //server running
 
 app.listen(port, () => {
---
+++ server/routes/captcha.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const pool = require("../db");
+
+module.exports = router;
