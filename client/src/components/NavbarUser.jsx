99db54f938a1a25468289b7a4a88f571514027d5
Swevosfr <90179383+Swevosfr@users.noreply.github.com>
2023-06-20 09:51:50 +0200

    css navbarclient

--- client/src/components/NavbarUser.jsx
+++ client/src/components/NavbarUser.jsx
@@ -26,14 +26,6 @@ const navigation = [
   { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
 ];
 
-const teams = [
-  { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
-
-  { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
-
-  { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
-];
-
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
@@ -100,8 +92,8 @@ export default function Example() {
 
                   {/* Sidebar component, swap this element with another sidebar if you like */}
 
-                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-2 ring-1 ring-white/10">
-                    <div className="flex h-16 shrink-0 items-center">
+                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-2 ring-1 ring-white/10 ">
+                    <div className="flex h-16 shrink-0 items-center ">
                       <img
                         className="h-8 w-auto"
                         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
@@ -109,7 +101,7 @@ export default function Example() {
                       />
                     </div>
 
-                    <nav className="flex flex-1 flex-col">
+                    <nav className="flex flex-1 flex-col ring-gray-200">
                       <ul role="list" className="flex flex-1 flex-col gap-y-7">
                         <li>
                           <ul role="list" className="-mx-2 space-y-1">
@@ -136,35 +128,6 @@ export default function Example() {
                             ))}
                           </ul>
                         </li>
-
-                        <li>
-                          <div className="text-xs font-semibold leading-6 text-gray-400">
-                            Your teams
-                          </div>
-
-                          <ul role="list" className="-mx-2 mt-2 space-y-1">
-                            {teams.map((team) => (
-                              <li key={team.name}>
-                                <a
-                                  href={team.href}
-                                  className={classNames(
-                                    team.current
-                                      ? "bg-gray-800 text-white"
-                                      : "text-gray-400 hover:text-white hover:bg-gray-800",
-
-                                    "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                                  )}
-                                >
-                                  <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                                    {team.initial}
-                                  </span>
-
-                                  <span className="truncate">{team.name}</span>
-                                </a>
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
                       </ul>
                     </nav>
                   </div>
@@ -176,10 +139,10 @@ export default function Example() {
 
         {/* Static sidebar for desktop */}
 
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col ">
+        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col ring-1 ring-white/10">
           {/* Sidebar component, swap this element with another sidebar if you like */}
 
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 ">
+          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6  ">
             <div className="flex h-16 shrink-0 items-center">
               <img
                 className="h-8 w-auto"
@@ -210,35 +173,6 @@ export default function Example() {
                           />
 
                           {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-
-                <li>
-                  <div className="text-xs font-semibold leading-6 text-gray-400">
-                    Your teams
-                  </div>
-
-                  <ul role="list" className="-mx-2 mt-2 space-y-1">
-                    {teams.map((team) => (
-                      <li key={team.name}>
-                        <a
-                          href={team.href}
-                          className={classNames(
-                            team.current
-                              ? "bg-gray-800 text-white"
-                              : "text-gray-400 hover:text-white hover:bg-gray-800",
-
-                            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                          )}
-                        >
-                          <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
-                            {team.initial}
-                          </span>
-
-                          <span className="truncate">{team.name}</span>
                         </a>
                       </li>
                     ))}
