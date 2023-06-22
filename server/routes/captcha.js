3ee89266a48b83b2e0b89512cf4909e6804a605d
Swevosfr <90179383+Swevosfr@users.noreply.github.com>
2023-06-22 23:28:50 +0200

    implementing uploads with multer

--- server/routes/captcha.js
+++ server/routes/captcha.js
@@ -1,6 +1,22 @@
 const router = require("express").Router();
 const pool = require("../db");
 const Authorization = require("../middleware/Authorization");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    if (req.body.question) {
+      cb(null, "./uploads/singulier");
+    } else {
+      cb(null, "./uploads/neutre");
+    }
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.get("/images", Authorization, async (req, res) => {});
 
