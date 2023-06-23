dc5442b25c7a66cf51dfdd19ec98d9feb263e10c
Swevosfr <90179383+Swevosfr@users.noreply.github.com>
2023-06-23 11:00:59 +0200

    route for uploading

--- server/routes/captcha.js
+++ server/routes/captcha.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const pool = require("../db");
 const Authorization = require("../middleware/Authorization");
 const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -12,12 +14,69 @@ const storage = multer.diskStorage({
     }
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + "-" + file.originalname);
+    cb(null, file.originalname);
   },
 });
 
 const upload = multer({ storage: storage });
 
-router.get("/images", Authorization, async (req, res) => {});
+router.get("/upload-images", Authorization, async (req, res) => {});
+
+router.get(
+  "/upload-images",
+  Authorization,
+  upload.array("image"),
+  async (req, res) => {}
+);
+
+router.post(
+  "/upload-images/:imageName/:id_captcha",
+  upload.single("imageFile"),
+  async (req, res) => {
+    try {
+      let imageName = req.params.imageName;
+      const question = req.body.question;
+      const id_captcha = req.params.id_captcha;
+
+      // Vérifiez le type d'image
+      if (!isValidImageFile(req.file)) {
+        return res.status(400).json({
+          message:
+            "Sélectionnez un fichier d'image au format .png, .jpg ou .jpeg",
+        });
+      }
+
+      let ext = path.extname(req.file.originalname).toLowerCase();
+      let newExt = path.extname(imageName).toLowerCase();
+
+      if (newExt !== ".png" && newExt !== ".jpg" && newExt !== ".jpeg") {
+        imageName += ext;
+      }
+
+      // Déplacez l'image téléchargée vers le bon dossier en fonction de la présence de la question
+      const destinationDir = question ? "singuliers" : "neutres";
+      const oldFilePath = req.file.path;
+      const newFilePath = path.join(
+        __dirname,
+        "..",
+        "upload",
+        destinationDir,
+        imageName
+      );
+      fs.renameSync(oldFilePath, newFilePath);
+
+      // Insérer la nouvelle image dans la base de données
+      const insertQuery =
+        "INSERT INTO Image (nom_image, id_captcha, question_associee) VALUES ($1, $2, $3)";
+      await pool.query(insertQuery, [imageName, id_captcha, question]);
+
+      // Succès
+      res.status(200).json({ message: "Image ajoutée avec succès" });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Erreur de serveur" });
+    }
+  }
+);
 
 module.exports = router;
