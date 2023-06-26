10f5cc045792515e9be1d2d01a960fe9a02b503c
Swevosfr <90179383+Swevosfr@users.noreply.github.com>
2023-06-26 02:14:34 +0200

    update adminArtiste

--- client/src/pages/admin/AdminRouter.jsx
+++ client/src/pages/admin/AdminRouter.jsx
@@ -4,6 +4,7 @@ import Error from "../../utils/Error";
 import LayoutAdmin from "./LayoutAdmin";
 import AdminDashboard from "../admin/adminDashboard/AdminDashboard";
 import AdminCaptcha from "../admin/adminCaptcha/AdminCaptcha";
+import AdminArtistes from "./adminArtistes/AdminArtistes";
 
 export default function AdminRouter() {
   return (
@@ -11,7 +12,8 @@ export default function AdminRouter() {
       <Route element={<LayoutAdmin />}>
         <Route index element={<AdminDashboard />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/admin/admin-captcha" element={<AdminCaptcha />} />
+        <Route path="/admin-captcha" element={<AdminCaptcha />} />
+        <Route path="/admin-artistes" element={<AdminArtistes />} />
         <Route path="*" element={<Error />} />
       </Route>
     </Routes>
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -15,4 +15,29 @@ router.post("/isAdmin", Authorization, async (req, res) => {
   }
 });
 
+router.post("/nameAdmin", Authorization, async (req, res) => {
+  try {
+    const user = await pool.query(
+      "SELECT user_first_name FROM users WHERE id_user = $1",
+      [req.user]
+    );
+    res.json(user.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json("Server Error");
+  }
+});
+
+router.get("/artistes", async (req, res) => {
+  try {
+    const artists = await pool.query(
+      "SELECT user_first_name, user_last_name, user_email FROM Users WHERE isAdmin = 0"
+    );
+    res.json(artists.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json("Server Error");
+  }
+});
+
 module.exports = router;
