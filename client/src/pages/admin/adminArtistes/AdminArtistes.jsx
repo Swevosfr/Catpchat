fe90e06a5a4a0356c7decbbdb68b459ec1edee12
Swevosfr <90179383+Swevosfr@users.noreply.github.com>
2023-06-26 02:22:02 +0200

    les artistes

--- client/src/pages/admin/adminArtistes/AdminArtistes.jsx
+++ client/src/pages/admin/adminArtistes/AdminArtistes.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { AiOutlineClose } from "react-icons/ai";
 
 const AdminArtistes = () => {
   const [artists, setArtists] = useState([]);
@@ -17,40 +18,14 @@ const AdminArtistes = () => {
     }
   };
 
+  const handleDeleteArtist = (id) => {
+    // Logique de suppression de l'artiste avec l'ID spécifié
+    console.log("Suppression de l'artiste avec l'ID :", id);
+  };
+
   return (
-    <div div className=" min-h-screen w-full bg-white">
-      <div className="relative isolate overflow-hidden  px-6 sm:py-32 lg:overflow-visible h-full w-full">
-        <div className="absolute inset-0 -z-10 overflow-hidden">
-          <svg
-            className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
-            aria-hidden="true"
-          >
-            <defs>
-              <pattern
-                id="e813992c-7d03-4cc4-a2bd-151760b470a0"
-                width={200}
-                height={200}
-                x="50%"
-                y={-1}
-                patternUnits="userSpaceOnUse"
-              >
-                <path d="M100 200V.5M.5 .5H200" fill="none" />
-              </pattern>
-            </defs>
-            <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
-              <path
-                d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
-                strokeWidth={0}
-              />
-            </svg>
-            <rect
-              width="100%"
-              height="100%"
-              strokeWidth={0}
-              fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)"
-            />
-          </svg>
-        </div>
+    <div className="min-h-screen bg-white">
+      <div className="container mx-auto py-10 px-6">
         <h1 className="text-2xl font-bold mb-4 text-center">
           Liste des Artistes
         </h1>
@@ -67,19 +42,30 @@ const AdminArtistes = () => {
                 <th className="py-3 px-4 border-b font-semibold text-sm text-black text-center">
                   Email
                 </th>
+                <th className="py-3 px-4 border-b font-semibold text-sm text-black text-center">
+                  Actions
+                </th>
               </tr>
             </thead>
             <tbody>
               {artists.map((artist) => (
                 <tr key={artist.user_email} className="border-b">
-                  <td className="py-3 px-4 text-sm text-black text-center border-r">
+                  <td className="py-3 px-4 text-sm text-black text-center">
                     {artist.user_first_name}
                   </td>
-                  <td className="py-3 px-4 text-sm text-black text-center border-r">
+                  <td className="py-3 px-4 text-sm text-black text-center">
                     {artist.user_last_name}
                   </td>
                   <td className="py-3 px-4 text-sm text-black text-center">
                     {artist.user_email}
+                  </td>
+                  <td className="py-3 px-4 text-sm text-center">
+                    <button
+                      onClick={() => handleDeleteArtist(artist.id_user)}
+                      className="text-red-600 hover:text-red-800 transition-colors duration-200"
+                    >
+                      <AiOutlineClose className="h-5 w-5 transform hover:scale-110" />
+                    </button>
                   </td>
                 </tr>
               ))}
